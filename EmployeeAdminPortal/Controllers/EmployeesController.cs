0e2c59b84375f86ec31b46375a634b2476ad439f
truongloc635 <truongquocloc635@gmail.com>
2026-04-02 09:16:19 +0700

    Add project files.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,80 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+            return Ok(allEmployees);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult UpdateEmployee(Guid id, EmployeeVM employee)
+        {
+            var employeeEntity = dbContext.Employees.Find(id);
+            if (employeeEntity is null)
+            {
+                return NotFound();
+            }
+            employeeEntity.Name = employee.Name;
+            employeeEntity.Email = employee.Email;
+            employeeEntity.Phone = employee.Phone;
+            employeeEntity.Salary = employee.Salary;
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(EmployeeVM employee)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = employee.Name,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Salary = employee.Salary
+            };
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult DeleteEmployee(Guid id)
+        {
+            var employeeEntity = dbContext.Employees.Find(id);
+            if (employeeEntity is null)
+            {
+                return NotFound();
+            }
+            dbContext.Employees.Remove(employeeEntity);
+            dbContext.SaveChanges();
+            return Ok(employeeEntity);
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Migrations/20260402013507_InitialPostgresCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeAdminPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgresCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    Salary = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
