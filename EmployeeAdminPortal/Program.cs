7c3169942461e114204703cba96c3f8921b7625e
truongloc635 <truongquocloc635@gmail.com>
2026-04-02 11:03:46 +0700

    con...

--- EmployeeAdminPortal/Program.cs
+++ EmployeeAdminPortal/Program.cs
@@ -1,33 +1,42 @@
-using EmployeeAdminPortal.Data;
+﻿using EmployeeAdminPortal.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()   
+              .AllowAnyMethod()   
+              .AllowAnyHeader();  
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<ApplicationDbContext>(options=>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")) 
-//);
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-if(!builder.Environment.IsDevelopment())
+
+if (!builder.Environment.IsDevelopment())
 {
     var envCon = Environment.GetEnvironmentVariable("DATABASE_URL");
-    if(!string.IsNullOrEmpty(envCon))
+    if (!string.IsNullOrEmpty(envCon))
     {
-        connectionString = envCon;
+        connectionString = envCon; 
     }
 }
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString(connectionString))
+    options.UseNpgsql(connectionString) // Đã sửa lỗi cú pháp ở đây
 );
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// 4. KÍCH HOẠT CORS (Bắt buộc phải nằm ở vị trí này)
+app.UseCors("AllowAll");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
